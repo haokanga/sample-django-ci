6aa021aacf7f5efee331ee98847b5041fb2647fc
Marshall An <haokanga@andrew.cmu.edu>
2024-12-02 13:14:01 -0500

    Corrected the failed test case

--- sample_project/sample_app/tests.py
+++ sample_project/sample_app/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from .models import Item
 
 class SimpleTestCase(TestCase):
-    def test_fix_this_failed_case(self):
+    def test_passed_case(self):
         """This test is intentionally incorrect to demonstrate a failed test case for correction."""
-        self.assertEqual(len("hello"), 4, "This test is designed to fail for demonstration purposes.")
+        self.assertEqual(len("hello"), 5, "This test is designed to fail for demonstration purposes.")
 
 
