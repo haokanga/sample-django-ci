d72752da47b2ce7c83890b65ec861c499a7f6c01
Marshall An <haokanga@andrew.cmu.edu>
2024-12-02 13:18:45 -0500

    This test is to demonstrate a passed test case.

--- sample_project/sample_app/tests.py
+++ sample_project/sample_app/tests.py
@@ -3,7 +3,5 @@ from .models import Item
 
 class SimpleTestCase(TestCase):
     def test_passed_case(self):
-        """This test is intentionally incorrect to demonstrate a failed test case for correction."""
-        self.assertEqual(len("hello"), 5, "This test is designed to fail for demonstration purposes.")
-
-
+        """This test is to demonstrate a passed test case."""
+        self.assertEqual(len("hello"), 5, "This test is designed to pass for demonstration purposes.")
