b0775d76633f99e5bc891ab12e1b5cdbf1a86fce
Marshall An <haokanga@andrew.cmu.edu>
2024-12-16 14:15:12 -0500

    This test is designed to pass for demonstration purposes.

--- sample_project/sample_app/tests.py
+++ sample_project/sample_app/tests.py
@@ -8,4 +8,4 @@ class SimpleTestCase(TestCase):
 
     def test_fix_this_failed_case(self):
         """This test is intentionally incorrect to demonstrate a failed test case for correction."""
-        self.assertEqual(len("EASI"), 3, "This test is designed to fail for demonstration purposes.")
+        self.assertEqual(len("EASI"), 4, "This test is designed to fail for demonstration purposes.")
